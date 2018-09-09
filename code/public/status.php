7f0ccc7c9be820be175ce935fc2b8d70611a1aac
Jez Humble <jez@jezhumble.net>
2018-09-08 21:20:44 -0700

    Updated Unit 6 to cover secrets

--- code/public/status.php
+++ code/public/status.php
@@ -2,16 +2,16 @@
 require __DIR__ . '/vendor/autoload.php';
 use Aws\Ssm\SsmClient;
 
-$parameters = "";
+$parameters = [];
 $path = "/";
 try {
     $client = new SsmClient(["version" => "latest", "region" => "us-east-1"]);
     $results = $client->getParametersByPath(['Path' => $path, 'WithDecryption' => true]);
     foreach ($results['Parameters'] as $parameter) {
 	$key = str_replace($path, "", $parameter['Name']);
-        $this->parameters[$key] = $parameter['Value'];
+        $parameters[$key] = $parameter['Value'];
     }
-    $connection = new PDO("mysql:host=".$parameters['DB_URI'].";dbname=jez-workshop;charset=utf8", "workshop", $parameters['DB_PASSWORD']);
+    $connection = new PDO("mysql:host=".$parameters['DB_URI'].";dbname=jezworkshop;charset=utf8", "workshop", $parameters['DB_PASSWORD']);
 } catch (Exception $e) {
     http_response_code(500);
     error_log("Couldn't create database connection. Error: ".$e->getMessage());
