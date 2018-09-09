11ce532d4fe7302326253d3967881835c4d70caf
Jez Humble <jez@jezhumble.net>
2018-09-08 18:11:01 -0700

    Fixed deploy script

--- code/deploy.sh
+++ code/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-tar -cjf latest.tar.bz2 app
+tar -cjf latest.tar.bz2 public
 MD5=`openssl md5 -binary latest.tar.bz2 | base64`
-aws s3api put-object --bucket jez-cloud-workshop --key latest.tar.bz2 --body latest.tar.bz2 --metadata md5chksum=$MD5 --content-md5 $MD5
 aws s3 sync scripts s3://jez-cloud-workshop/
+aws s3api put-object --bucket jez-cloud-workshop --key latest.tar.bz2 --body latest.tar.bz2 --metadata md5chksum=$MD5
